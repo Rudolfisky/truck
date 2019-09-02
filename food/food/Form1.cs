e58e8b4fde087a97ec1a4672956574ffbfa1a9e6
Rudolfisky <33927733+Rudolfisky@users.noreply.github.com>
2019-09-02 10:08:56 +0200

    Refresh Class
    speed, momentum, efficient usses of classes

--- food/food/Form1.cs
+++ food/food/Form1.cs
@@ -13,24 +13,12 @@ namespace food
 {
     public partial class Form1 : Form
     {
+        DataSet LocalDS = new DataSet();
+
         public Form1()
         {
             InitializeComponent();
-            try
-            {
-                MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=");
-                MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM eet_wagen.product", connection);
-
-                connection.Open();
-
-                DataSet ds = new DataSet();
-                adapter.Fill(ds, "product");
-                dataGridView1.DataSource = ds.Tables["product"];
-                connection.Close();
-            } catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            Refresh();
         }
 
         private void contextMenuStrip1_Opening(object sender, EventArgs e)
@@ -38,18 +26,20 @@ namespace food
             System.Diagnostics.Debug.WriteLine("test");
         }
 
-        private void Refresh_Click(object sender, EventArgs e)
+        public void Refresh()
         {
             try
             {
+                LocalDS = new DataSet();
                 MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=");
                 MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM eet_wagen.product", connection);
 
                 connection.Open();
 
-                DataSet ds = new DataSet();
-                adapter.Fill(ds, "product");
-                dataGridView1.DataSource = ds.Tables["product"];
+                adapter.Fill(LocalDS, "product");
+                System.Diagnostics.Debug.WriteLine(LocalDS);
+                dataGridView1.DataSource = LocalDS.Tables["product"];
+
                 connection.Close();
             }
             catch (Exception ex)
@@ -58,14 +48,33 @@ namespace food
             }
         }
 
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
         private void del_Click(object sender, EventArgs e)
         {
-
         }
 
         private void upload_Click(object sender, EventArgs e)
         {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=");
+                MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM eet_wagen.product", connection);
 
+                connection.Open();
+
+                //DataSet ds = new DataSet();
+                //adapter.Fill(ds, "product");
+                //dataGridView1.DataSource = ds.Tables["product"];
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
