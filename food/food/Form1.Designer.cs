63d54701309b0635036a7a0e766ca27366e6ea44
Niels <nielsmath@live.nl>
2019-09-02 10:11:24 +0200

    sorteer box

--- food/food/Form1.Designer.cs
+++ food/food/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.refresh = new System.Windows.Forms.Button();
             this.del = new System.Windows.Forms.Button();
             this.upload = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,17 +50,19 @@
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 71);
+            this.dataGridView1.Location = new System.Drawing.Point(9, 58);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(612, 280);
+            this.dataGridView1.Size = new System.Drawing.Size(459, 228);
             this.dataGridView1.TabIndex = 1;
             // 
             // refresh
             // 
-            this.refresh.Location = new System.Drawing.Point(12, 12);
+            this.refresh.Location = new System.Drawing.Point(9, 10);
+            this.refresh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.refresh.Name = "refresh";
-            this.refresh.Size = new System.Drawing.Size(101, 42);
+            this.refresh.Size = new System.Drawing.Size(76, 34);
             this.refresh.TabIndex = 2;
             this.refresh.Text = "refresh";
             this.refresh.UseVisualStyleBackColor = true;
@@ -67,9 +70,10 @@
             // 
             // del
             // 
-            this.del.Location = new System.Drawing.Point(227, 12);
+            this.del.Location = new System.Drawing.Point(170, 10);
+            this.del.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.del.Name = "del";
-            this.del.Size = new System.Drawing.Size(101, 42);
+            this.del.Size = new System.Drawing.Size(76, 34);
             this.del.TabIndex = 3;
             this.del.Text = "delete";
             this.del.UseVisualStyleBackColor = true;
@@ -77,23 +81,34 @@
             // 
             // upload
             // 
-            this.upload.Location = new System.Drawing.Point(120, 12);
+            this.upload.Location = new System.Drawing.Point(90, 10);
+            this.upload.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.upload.Name = "upload";
-            this.upload.Size = new System.Drawing.Size(101, 42);
+            this.upload.Size = new System.Drawing.Size(76, 34);
             this.upload.TabIndex = 4;
             this.upload.Text = "upload";
             this.upload.UseVisualStyleBackColor = true;
             this.upload.Click += new System.EventHandler(this.upload_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(251, 18);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(641, 368);
+            this.ClientSize = new System.Drawing.Size(481, 299);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.upload);
             this.Controls.Add(this.del);
             this.Controls.Add(this.refresh);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -107,6 +122,7 @@
         private System.Windows.Forms.Button refresh;
         private System.Windows.Forms.Button del;
         private System.Windows.Forms.Button upload;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
